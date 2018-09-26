53373ea9ccdf5a1d5f3a97fc94bc12d0b57c4cdc
deshmukhsanket <sanketdeshmukh7206@gmail.com>
2018-09-26 11:39:56 +0530

    DONE Changes In lib folder

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ const app = express()
 const port = 3000
 
 import "./config/globals"
-import "./lib/schemas.js";
+import "./lib/controllers.js"
+import "./lib/express.js"
+import "./lib/models.js"
+import "./lib/mongoose.js"
+import "./lib/responses.js"
+import "./lib/schemas.js"
+import "./lib/services.js"
 app.listen(port, () => {
     console.log(`Server Started at Port ${port}`)
 })
