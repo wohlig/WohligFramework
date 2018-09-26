08a83387d410501aa7dc3147ee22d6c641fb6750
Chintan Shah <chintan@wohlig.com>
2018-09-26 17:18:21 +0530

    changes

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 import express from "express"
 const app = express()
 const port = 3000
-
+import "./lib/express.js"
 import "./config/globals"
 import "./lib/controllers.js"
-import "./lib/express.js"
 import "./lib/models.js"
-import "./lib/mongoose.js"
+// import "./lib/mongoose.js"
 import "./lib/responses.js"
 import "./lib/schemas.js"
-import "./lib/services.js"
+// import "./lib/services.js"
 app.listen(port, () => {
     console.log(`Server Started at Port ${port}`)
 })
