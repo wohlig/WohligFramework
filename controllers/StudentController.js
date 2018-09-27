4d194761f7e69ba67fb55cf8fbcfe0e8b180c702
Chintan Shah <chintan@wohlig.com>
2018-09-27 18:42:40 +0530

    ok

--- controllers/StudentController.js
+++ controllers/StudentController.js
@@ -1,20 +1,20 @@
 const router = Router()
-router.get('/', function (req, res) {
-    StudentModel.search(req.query, res.callback);
+router.get("/", function (req, res) {
+    StudentModel.search(req.query, res.callback)
 })
-router.get('/:id', function (req, res) {
+router.get("/:id", function (req, res) {
     res.send(`Get For Id ${req.params.id}`)
 })
-router.post('/', function (req, res) {
-    res.send(`Save Data`)
+router.post("/", function (req, res) {
+    res.send("Save Data")
 })
-router.put('/:id', function (req, res) {
+router.put("/:id", function (req, res) {
     res.send(`Update For Id ${req.params.id}`)
 })
-router.patch('/:id', function (req, res) {
+router.patch("/:id", function (req, res) {
     res.send(`Path For Id ${req.params.id}`)
 })
-router.delete('/:id', function (req, res) {
+router.delete("/:id", function (req, res) {
     res.send(`Delete For Id ${req.params.id}`)
 })
-export default router;
+export default router
