2e6b3d806307021debfc8dd468fd0a73c652e9ff
Chintan Shah <chintan@wohlig.com>
2018-09-27 16:12:12 +0530

    changes for the env

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-import "./lib/express.js"
+process.env.NODE_ENV = !process.env.NODE_ENV ? "development" : process.env.NODE_ENV
+global.env = require("./config/env/" + process.env.NODE_ENV)
 import "./config/globals"
-import "./lib/controllers.js"
 import "./lib/mongoose.js"
+import "./lib/express.js"
+import "./lib/controllers.js"
 import "./lib/models.js"
 import "./lib/responses.js"
 import "./lib/services.js"
+import "./config/middleware.js"
 app.listen(port, () => {
     console.log(`Server Started at Port ${port}`)
 })
