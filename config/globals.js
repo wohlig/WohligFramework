715a85d046a0afc53421500118b8f2f467f4a52e
Chintan Shah <chintan@wohlig.com>
2018-09-28 17:39:11 +0530

    ok

--- config/globals.js
+++ config/globals.js
@@ -2,10 +2,3 @@
  * Define Global Variables Here
  * global._ = require("lodash")
  */
-
-// ajv.addKeyword("range", {
-//     type: "string",
-//     compile: (data, parentSchema) => {
-
-//     }
-// })
