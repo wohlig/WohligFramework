a53ed9857aaa5d65d3eb565a12bbc3a0d062f324
jagzwohlig <jagruti@wohlig.com>
2019-01-29 16:04:26 +0530

    production atlas database added

--- config/env/production.js
+++ config/env/production.js
@@ -1,5 +1,6 @@
 export default {
     port: 3000,
-    mongodbUrl: "mongodb://localhost:27017/wohligFramework?readPreference=primary",
+    mongodbUrl:
+        "mongodb+srv://admin:admin@cluster0-vjbdu.gcp.mongodb.net/wohligFramework",
     url: "http://wohlig.io"
 }
