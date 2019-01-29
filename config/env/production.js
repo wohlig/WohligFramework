0062611fe0a2eea39ea8d792a505c461b24e259f
jagzwohlig <jagruti@wohlig.com>
2019-01-29 16:38:36 +0530

    changes for the url

--- config/env/production.js
+++ config/env/production.js
@@ -2,5 +2,5 @@ export default {
     port: 3000,
     mongodbUrl:
         "mongodb+srv://admin:admin@cluster0-vjbdu.gcp.mongodb.net/wohligFramework",
-    url: "http://wohlig.io"
+    url: "https://wohligframework.wohlig.in"
 }
