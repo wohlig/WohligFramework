42f06f034564ce2ba8391411428fb849497b88b0
Chintan Shah <chintan@wohlig.com>
2019-07-15 12:31:29 +0530

    ok

--- config/routes.js
+++ config/routes.js
@@ -8,4 +8,23 @@ router.get("/", (req, res) => {
         name: "Wohlig Framework"
     })
 })
+router.get("/ip", (req, res) => {
+    var request = require("request")
+
+    var options = {
+        method: "GET",
+        url: "https://api.ipify.org/",
+        qs: { format: "json" }
+    }
+
+    request(options, function(error, response, body) {
+        if (err) {
+            res.callback(err)
+        } else if (body) {
+            res.callback(null, JSON.parse(body))
+        } else {
+            res.callback()
+        }
+    })
+})
 export default router
