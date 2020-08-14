dbe7acd64a78ce59e69e664ecc546b7b4f279643
Chintan Shah <chintan@wohlig.com>
2020-08-15 00:10:25 +0530

    changes

--- config/env/production.js
+++
@@ -1,6 +0,0 @@
-export default {
-    port: 3000,
-    mongodbUrl:
-        "mongodb+srv://admin:admin@cluster0-vjbdu.gcp.mongodb.net/wohligFramework?readPreference=primary",
-    url: "https://wohligframework.wohlig.in"
-}
