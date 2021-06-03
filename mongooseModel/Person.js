8d35707f1d0dac01b0e4f7e2981aa7c849e68b4d
Chintan Shah <chintan@wohlig.com>
2021-06-03 21:51:17 +0530

    changes

--- mongooseModel/Person.js
+++ mongooseModel/Person.js
@@ -1,7 +1,13 @@
 var schema = new Schema(
     {
         name: { type: String, index: true },
-        email: { type: String }
+        email: { type: String },
+        status: {
+            type: String,
+            enum: ["enabled", "disabled", "archived"],
+            default: "enabled",
+            index: true
+        }
     },
     {
         timestamp: true
