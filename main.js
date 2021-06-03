1b43eeb7db1056d5cb8d04586dab6e51308af897
Chintan Shah <chintan@wohlig.com>
2021-06-03 13:36:17 +0530

    changes for the esm

---
+++ main.js
@@ -0,0 +1,17 @@
+require("dotenv").config()
+global.pwd = __dirname
+global.env = process.env
+require("./lib/globals")
+require(pwd + "/config/globals")
+require("./lib/mongoose.js")
+require("./lib/express.js")
+require(pwd + "/config/middleware.js")
+require("./lib/responses.js")
+require("./lib/controllers.js")
+require("./lib/models.js")
+require("./lib/views.js")
+
+app.listen(env["PORT"], () => {
+    console.log(`Server Started at Port ${env["PORT"]}`)
+    require(pwd + "/config/cron")
+})
