207f6ac85e0c1a4e68ca2ab0601a9c318f7423b0
Chintan Shah <chintan@wohlig.com>
2021-06-03 13:32:20 +0530

    changes

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-require("@babel/register")({
-    presets: ["@babel/preset-env"],
-    plugins: [
-        "@babel/plugin-transform-runtime",
-        "@babel/plugin-transform-async-to-generator"
-    ]
+require("dotenv").config()
+global.pwd = __dirname
+global.env = process.env
+require("./lib/globals")
+require(pwd + "/config/globals")
+require("./lib/mongoose.js")
+require("./lib/express.js")
+require(pwd + "/config/middleware.js")
+require("./lib/responses.js")
+require("./lib/controllers.js")
+require("./lib/models.js")
+require("./lib/views.js")
+
+app.listen(env["PORT"], () => {
+    console.log(`Server Started at Port ${env["PORT"]}`)
+    require(pwd + "/config/cron")
 })
-require("wohlig-framework-core")
